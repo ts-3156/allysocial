22133ceadde477f05bee33db00777bd492d87bba
ts-3156 <ts_3156@yahoo.co.jp>
2021-01-05 21:21:33 +0900

    Add twitter link and DM link

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -2,4 +2,12 @@ module PathsHelper
   def egotter_url(user)
     "https://egotter.com/timelines/#{user}"
   end
+
+  def twitter_url(user)
+    "https://twitter.com/#{user}"
+  end
+
+  def direct_message_url(sender_uid, recipient_uid)
+    "https://twitter.com/messages/#{sender_uid}-#{recipient_uid}"
+  end
 end
