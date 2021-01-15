f780110d72ee4488cf54363e84185cdaf77d59cc
ts-3156 <ts_3156@yahoo.co.jp>
2021-01-15 23:27:51 +0900

    Implement Agent.dump

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -18,5 +18,13 @@ class Agent < ApplicationRecord
     def api_client
       find(pluck(:id).sample).api_client
     end
+
+    def dump
+      all.map do |agent|
+        user = agent.user
+        credential = user.credential
+        [user.uid, user.screen_name, credential.access_token, credential.access_secret].join(',')
+      end.join("\n")
+    end
   end
 end
